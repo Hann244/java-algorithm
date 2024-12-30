84cb7a4992e5d589063273bf1809016f2bc9cc47
onhann <gusgus1467@naver.com>
2024-12-30 11:40:30 +0900

    [Bronze V] Title: 문자열, Time: 164 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        String[] arr = new String[T];
+
+        for (int i = 0; i < T; i++) {
+            arr[i]= sc.next();
+        }
+
+        for (int i = 0; i < T; i++) {
+            System.out.print(arr[i].charAt(0));
+            System.out.print(arr[i].charAt(arr[i].length() - 1));
+            System.out.println();
+        }
+    }
+}
