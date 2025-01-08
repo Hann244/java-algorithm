dcce1a1fd2426f2e98fb28e406eccf4628e4fa24
onhann <gusgus1467@naver.com>
2025-01-08 11:41:31 +0900

    [Bronze I] Title: 소인수분해, Time: 160 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        if (N == 1) System.out.println(" ");
+
+        int i = 2;
+        while (N > 1) {
+            if (N % i == 0) {
+                System.out.println(i);
+                N /= i;
+            }
+            if (N % i != 0) {
+                i++;
+            }
+        }
+    }
+}
