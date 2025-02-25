fafa7b39a917fee304f343b281859b057484bfd9
onhann <gusgus1467@naver.com>
2025-02-25 11:23:21 +0900

    [Silver IV] Title: 접미사 배열, Time: 168 ms, Memory: 16052 KB -BaekjoonHub

--- 백준/Silver/11656. 접미사 배열/접미사 배열.java
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.java
@@ -10,7 +10,7 @@ public class Main {
 		String[] arr = new String[str.length()];
 		
 		for (int i = 0; i < str.length(); i++) {
-			arr[i] = str.substring(str.length() - (i + 1), str.length());
+			arr[i] = str.substring(i, str.length());
 		}
 		
 		Arrays.sort(arr);
