e037b473304b10c6dfa58e1e57f4787c2dca1367
onhann <gusgus1467@naver.com>
2025-03-26 11:21:00 +0900

    [Bronze III] Title: 네 번째 점, Time: 192 ms, Memory: 18380 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x1 = sc.nextInt();
+		int y1 = sc.nextInt();
+		
+		int x2 = sc.nextInt();
+		int y2 = sc.nextInt();
+		
+		int x3 = sc.nextInt();
+		int y3 = sc.nextInt();
+		
+		int x4 = 0, y4 = 0;
+		if (x1 != x2 && x1 != x3) x4 = x1;
+		else if (x2 != x1 && x2 != x3) x4 = x2;
+		else x4 = x3;
+		
+		if (y1 != y2 && y1 != y3) y4 = y1;
+		else if (y2 != y1 && y2 != y3) y4 = y2;
+		else y4 = y3;
+		
+		System.out.println(x4 + " " + y4);
+	}
+
+}
