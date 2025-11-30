f0f5a97ec8cf6ea50fdce7ad651ee984b8e69f8c
onhann <gusgus1467@naver.com>
2025-11-30 21:07:59 +0900

    [Silver IV] Title: 스택, Time: 176 ms, Memory: 19028 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,48 +1,52 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		ArrayList<Integer> stack = new ArrayList<Integer>();
+		
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String text = st.nextToken();
+			
+			if (text.equals("push")) {
+				stack.add(Integer.parseInt(st.nextToken()));
+			} else if (text.equals("pop")) {
+				if (stack.isEmpty()) {
+					bw.write(String.valueOf(-1) + "\n");
+					continue;
+				}
+				bw.write(String.valueOf(stack.remove(stack.size() - 1)) + "\n");
+			} else if (text.equals("size")) {
+				bw.write(String.valueOf(stack.size()) + "\n");
+			} else if (text.equals("empty")) {
+				if (stack.isEmpty()) {
+					bw.write(String.valueOf(1) + "\n");
+				} else {
+					bw.write(String.valueOf(0) + "\n");
+				}
+			} else if (text.equals("top")) {
+				if (stack.isEmpty()) {
+					bw.write(String.valueOf(-1) + "\n");
+					continue;
+				}
+				bw.write(String.valueOf(stack.get(stack.size() - 1)) + "\n");
+			}
+		}
 
-        int N = Integer.parseInt(br.readLine());
-        ArrayList<Integer> nums = new ArrayList<>();
+		bw.flush();
+		bw.close();
+	}
 
-        for (int i = 0; i < N; i++) {
-            String command = br.readLine();
-
-            if (command.contains("push")) {
-                nums.add(Integer.parseInt(command.split(" ")[1]));
-            }
-            if (command.equals("pop")) {
-                if (nums.isEmpty()) {
-                    System.out.println(-1);
-                }
-                if (!nums.isEmpty()) {
-                    System.out.println(nums.remove(nums.size() - 1));
-                }
-            }
-            if (command.equals("size")) {
-                System.out.println(nums.size());
-            }
-            if (command.equals("empty")) {
-                if (!nums.isEmpty()) {
-                    System.out.println(0);
-                }
-                if (nums.isEmpty()) {
-                    System.out.println(1);
-                }
-            }
-            if (command.equals("top")) {
-                if (!nums.isEmpty()) {
-                    System.out.println(nums.get(nums.size() - 1));
-                }
-                if (nums.isEmpty()) {
-                    System.out.println(-1);
-                }
-            }
-        }
-    }
 }
