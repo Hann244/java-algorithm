e2b0cbc77033b43e8127c07fb338fa31db93b6fb
onhann <gusgus1467@naver.com>
2025-12-01 11:45:53 +0900

    [Silver IV] Title: 큐, Time: 192 ms, Memory: 19564 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,51 +1,56 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int N = Integer.parseInt(br.readLine());
-		ArrayList<Integer> queue = new ArrayList<>();
+		LinkedList<Integer> queue = new LinkedList<>();
 		
 		for (int i = 0; i < N; i++) {
-			String str = br.readLine();
-			if (str.contains("push")) {
-				queue.add(Integer.parseInt(str.split(" ")[1]));
-			}
-			if (str.equals("pop")) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String command = st.nextToken();
+			
+			if (command.equals("push")) {
+				queue.add(Integer.parseInt(st.nextToken()));
+			} else if (command.equals("pop")) {
 				if (queue.isEmpty()) {
-					System.out.println(-1);
-				} else {
-					System.out.println(queue.remove(0));
+					bw.write(-1 + "\n");
+					continue;
 				}
-			}
-			if (str.equals("size")) {
-				System.out.println(queue.size());
-			}
-			if (str.equals("empty")) {
+				bw.write(queue.remove() + "\n");
+			} else if (command.equals("size")) {
+				bw.write(queue.size() + "\n");
+			} else if (command.equals("empty")) {
 				if (queue.isEmpty()) {
-					System.out.println(1);
+					bw.write(1 + "\n");
 				} else {
-					System.out.println(0);
+					bw.write(0 + "\n");
 				}
-			}
-			if (str.equals("front")) {
+			} else if (command.equals("front")) {
 				if (queue.isEmpty()) {
-					System.out.println(-1);
-				} else {
-					System.out.println(queue.get(0));
+					bw.write(-1 + "\n");
+					continue;
 				}
-			}
-			if (str.equals("back")) {
+				bw.write(queue.get(0) + "\n");
+			} else if (command.equals("back")) {
 				if (queue.isEmpty()) {
-					System.out.println(-1);
-				} else {
-					System.out.println(queue.get(queue.size() - 1));
+					bw.write(-1 + "\n");
+					continue;
 				}
+				bw.write(queue.get(queue.size() - 1) + "\n");
 			}
 		}
+		
+		bw.close();
 	}
 
 }
