3c721c80aa52f1678809d0ac8d1e5174690a55f4
onhann <gusgus1467@naver.com>
2025-12-08 09:32:01 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        Integer[] coins = new Integer[N];
+
+        for (int i = 0; i < N; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(coins, Collections.reverseOrder());
+
+        int count = 0;
+        for (int coin : coins) {
+            count += K / coin;
+            K = K % coin;
+            if (K == 0) break;
+        }
+
+        System.out.println(count);
+    }
+}
