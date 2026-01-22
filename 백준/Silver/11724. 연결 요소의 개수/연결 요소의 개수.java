3ab0e200bbf1ab9bf37c9ac4d17aef45aea19c21
onhann <gusgus1467@naver.com>
2026-01-22 15:46:02 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 404 ms, Memory: 116924 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,50 +1,46 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 	
-	static ArrayList<Integer>A[];
-	static boolean[] visited;
+	static int graph[][];
+	static boolean visited[];
+	static int cnt = 0;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		A = new ArrayList[N + 1];
-		visited = new boolean[N + 1];
-		
-		for (int i = 1; i <= N; i++) {
-			A[i] = new ArrayList<Integer>();
-		}
-		
-		for (int i = 0; i < M; i++) {
+		graph = new int[N + 1][N + 1];
+		for (int i = 1; i <= M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int u = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
-			A[u].add(v);
-			A[v].add(u);
+			graph[u][v] = graph[v][u] = 1;
 		}
 		
-		int count = 0;
-		
+		visited = new boolean[N + 1];
 		for (int i = 1; i <= N; i++) {
 			if (!visited[i]) {
-				count++;
-				DFS(i);
+				dfs(i);
+				cnt++;
 			}
 		}
 		
-		System.out.print(count);
+		System.out.println(cnt);
 	}
-
-	private static void DFS(int node) {
-		visited[node] = true;
-		for (int i : A[node]) {
-			if (!visited[i]) {
-				DFS(i);
+	
+	static void dfs(int start) {
+		visited[start] = true;
+		
+		for (int i = 1; i < graph.length; i++) {
+			if (!visited[i] && graph[start][i] == 1) {
+				dfs(i);
 			}
 		}
 	}
