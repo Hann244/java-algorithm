28094b2f2d0b9aab86b0dfa858b8c4bf185a2956
onhann <gusgus1467@naver.com>
2026-03-30 21:10:53 +0900

    [Silver IV] Title: 설탕 배달, Time: 112 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,24 +1,35 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
-        int count = 0;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		// 사용한 봉지의 개수
+		int cnt = 0;
+		
+		// 설탕을 다 담을 때까지(n이 0이 될 때까지)
+		// 혹은 정확히 담을 수 없어 음수가 될 때까지 계속 반복
+		while (n > 0) {
+			
+			// 1. 가장 무거운 5kg 봉지로만 다 담을 수 있는지 확인
+			if (n % 5 == 0) {
+				cnt++;
+				n -= 5;
+			} else {
+				cnt++;
+				n -= 3;
+			}
+		}
+		
+		if (n != 0) {
+			System.out.println(-1);
+		} else {
+			System.out.println(cnt);
+		}
+	}
 
-        while (N > 0) {
-            if (N % 5 == 0) {
-                count += N / 5;
-                break;
-            } else {
-                N -= 3;
-                count++;
-            }
-            if (N < 0) {
-                count = -1;
-            }
-        }
-        System.out.println(count);
-    }
 }
