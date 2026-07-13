bd7c66a2a10b37ed4ae21a3c21789aae4aa592cb
onhann <gusgus1467@naver.com>
2026-07-13 21:51:12 +0900

    [level 1] Title: 폰켓몬, Time: 4.25 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,21 +1,15 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
         
-        Set<Integer> set = new HashSet<>();
-        for (int i : nums) {
-            set.add(i);
-        }
+        Set<Integer> set = Arrays.stream(nums)
+                                .boxed()
+                                .collect(Collectors.toSet());
         
-        
-        if (set.size() < (nums.length / 2)) {
-            answer = set.size();
-        } else {
-            answer = nums.length / 2;
-        }
-        
-        return answer;
+        return set.size() > (nums.length / 2) ? nums.length / 2 : set.size();
     }
 }
