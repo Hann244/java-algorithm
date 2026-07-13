016e3b3911d01e1a189d3f9d52d6af8a71c28a38
onhann <gusgus1467@naver.com>
2026-07-13 22:19:11 +0900

    [level 1] Title: 추억 점수, Time: 1.11 ms, Memory: 93 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -4,24 +4,20 @@ class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
         int[] answer = new int[photo.length];
         
-        // 1. 이름표가 붙은 사물함(HashMap) 만들기
-        // Key는 사람 이름(String), Value는 추억 점수(Integer)
-        Map<String, Integer> scoreMap = new HashMap<>();
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
         
-        // 2. 사물함에 데이터 채워넣기
         for (int i = 0; i < name.length; i++) {
-            scoreMap.put(name[i], yearning[i]);
+            map.put(name[i], yearning[i]);
         }
         
-        // 3. 점수 계산하기
         for (int i = 0; i < photo.length; i++) {
-            int tmp = 0;
-            for (String person : photo[i]) {
-                // 사물함에서 person의 이름을 찾아 점수를 꺼냄 (없으면 기본값 0)
-                // getOrDefault: 찾는 Key가 Map에 있으면 그 Value를 주고, 없으면 뒤에 적은 기본값(0)을 줍니다.
-                tmp += scoreMap.getOrDefault(person, 0);
+            int temp = 0;
+            for (String p : photo[i]) {
+                if (map.containsKey(p)) {
+                    temp += map.get(p);
+                }
             }
-            answer[i] = tmp;
+            answer[i] = temp;
         }
         
         return answer;
