557d57628ccdee4801e958efbb656d1b53bead6c
onhann <gusgus1467@naver.com>
2026-07-19 20:57:45 +0900

    [level 1] Title: 2016년, Time: 33.68 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,14 +1,13 @@
 import java.time.*;
-import java.time.format.TextStyle;
-import java.util.Locale;
+import java.time.format.*;
+import java.util.*;
 
 class Solution {
     public String solution(int a, int b) {
         
         LocalDate date = LocalDate.of(2016, a, b);
         DayOfWeek dayOfWeek = date.getDayOfWeek();
-        
-        String answer = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.US).toUpperCase();
-        return answer;
+            
+        return dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.US).toUpperCase();
     }
 }
